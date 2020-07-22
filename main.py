ce7c689308d769bd2efe2b77f23d2d15e16b2e03
ashdaily <ashishsinghbardhan@gmail.com>
2020-07-23 04:27:46 +0900

    use arg parse for entering credentials

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
+import argparse
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome()
 
-#Auto login linkedin
-driver.get("https://www.linkedin.com/login")
+class LinkedIn:
+    def __init__(self, username, password):
+        self.username = username
+        self.__password = password
+        self.driver = webdriver.Chrome()
 
-username = driver.find_element_by_id("username")
-username.send_keys("put your email here")
+    def get_password(self):
+        return self.__password
 
-password = driver.find_element_by_id("password")
-password.send_keys("put your password here")
-password.send_keys(Keys.RETURN)
+    def login(self):
+        self.driver.get("https://www.linkedin.com/login")
+
+        username = self.driver.find_element_by_id("username")
+        username.send_keys(self.username)
+        
+        password = self.driver.find_element_by_id("password")
+        password.send_keys(self.get_password())
+        password.send_keys(Keys.RETURN)      
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--username', help='linkedin username', type= str)
+    parser.add_argument('--password', help='linkedin password', type= str)
+    args = parser.parse_args() 
+
+    account = LinkedIn(args.username, args.password)
+    account.login()
